8208b5483ed42d2452d571ba1738d2a65b9063fb
Agustin Zamar <agustinzamar33@gmail.com>
2020-08-17 18:51:06 -0300

    ssr configurations ready

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,15 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import webpack from 'webpack'
+import React from 'react'
+import { renderToString } from 'react-dom/server'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import { StaticRouter } from 'react-router-dom'
+import serverRoutes from '../frontend/routes/ServerRoutes'
+import reducer from '../frontend/reducers/index'
+import initialState from '../frontend/initialState'
+import { renderRoutes } from 'react-router-config'
 
 dotenv.config()
 
@@ -9,12 +19,53 @@ const app = express()
 
 if (ENV === 'development') {
     console.log('Development config')
+    const webpackConfig = require('../../webpack.config')
+    const webpackDevMiddleware = require('webpack-dev-middleware')
+    const webpackHotMiddleware = require('webpack-hot-middleware')
+    const compiler = webpack(webpackConfig)
+    const serverConfig = { port: PORT, hot: true }
+
+    app.use(webpackDevMiddleware(compiler, serverConfig))
+    app.use(webpackHotMiddleware(compiler))
 }
 
-app.get('*', (req, res) => {
-    console.log('hola')
-    res.send({ hello: 'express' })
-})
+const setResponse = (html, preloadedState) => {
+    return `<!DOCTYPE html>
+	<html>
+		<head>
+			<link href="assets/app.css" rel="stylesheet"/>	
+			<title>Platzi Video</title>
+		</head>
+		<body>
+			<div id="app">
+			 ${html}
+			</div>
+			<script>
+				window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(
+                    /</g,
+                    '\\u003c'
+                )}
+			</script>
+			<script src="assets/app.js" type="text/javascript"></script>
+		</body>
+	</html>`
+}
+
+const renderApp = (req, res) => {
+    const store = createStore(reducer, initialState)
+    const preloadedState = store.getState()
+    const html = renderToString(
+        <Provider store={store}>
+            <StaticRouter location={req.url} context={{}}>
+                {renderRoutes(serverRoutes)}
+            </StaticRouter>
+        </Provider>
+    )
+
+    res.send(setResponse(html, preloadedState))
+}
+
+app.get('*', renderApp)
 
 app.listen(PORT, err => {
     if (err) console.log(err)
--- webpack.config.js
+++ webpack.config.js
@@ -1,67 +1,68 @@
-const path = require('path');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const webpack = require('webpack')
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/',
-  },
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'html-loader'
-          }
-        ]
-      },
-      {
-        test: /\.(s*)css$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-          },
-          'css-loader',
-          'sass-loader'
-        ]
-      },
-      {
-        test: /\.(png|gif|jpg)$/,
-        use: [
-          {
-            'loader': 'file-loader',
-            options: {
-              name: 'assets/[hash].[ext]'
-            }
-          }
-        ]
-      }
-    ]
-  },
-  devServer: {
-    historyApiFallback: true,
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: './public/index.html',
-      filename: './index.html'
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'assets/[name].css'
-    }),
-  ]
-};
+    entry: [
+        './src/frontend/index.js',
+        'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=2000&reload=true',
+    ],
+    mode: 'development',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'assets/app.js',
+        publicPath: '/',
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                    },
+                ],
+            },
+            {
+                test: /\.(s*)css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.(png|gif|jpg)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'assets/[hash].[ext]',
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    devServer: {
+        historyApiFallback: true,
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'assets/app.css',
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+    ],
+}
